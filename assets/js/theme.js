7549716f05f26cbaa23d35fe9119ce1e1fd809cc
Elodie Gateau <elodie.gateau.raffeneau@gmail.com>
2025-09-18 14:22:27 +0200

    Modification fichiers theme sass

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,3 +1,4 @@
+// Définition des variables
 const themeToggle = document.getElementById("themeToggle");
 const themeToggleOn = document.getElementById("iconOn");
 const themeToggleOff = document.getElementById("iconOff");
@@ -6,27 +7,28 @@ const moon = document.getElementById("moon");
 const html = document.documentElement;
 const badgeWSCarbon = document.getElementById("wcb");
 
+// Récupération du thème stockée dans le local Storage
 const themeStorage = localStorage.getItem("theme");
+
+// Si le thème enregistré était "eco" application du thème eco
 if (themeStorage === "eco") {
     html.classList.add("eco");
     themeToggleOn.classList.remove("hidden");
     themeToggleOff.classList.add("hidden");
     sun.classList.add("hidden");
     moon.classList.remove("hidden");
+    badgeWSCarbon.classList.add("wcb-d");
 }
 
+// Application du thème selon le click du toggle
 if (themeToggle) {
-    console.log("JS theme chargé");
     themeToggle.addEventListener("click", () => {
         const eco = html.classList.toggle("eco");
         themeToggleOn.classList.toggle("hidden", !eco);
         themeToggleOff.classList.toggle("hidden", eco);
         sun.classList.toggle("hidden", eco);
         moon.classList.toggle("hidden", !eco);
-        badgeWSCarbon.classList.toggle("wcb-d", !eco);
-
+        badgeWSCarbon.classList.toggle("wcb-d", eco);
         localStorage.setItem("theme", eco ? "eco" : "default");
     });
-} else {
-    console.log("JS theme chargé mais pas de bouton trouvé");
 }
