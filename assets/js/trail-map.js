fcdf4e1227f9b020656cc2a992f2af82d3d5d8bf
Elodie Gateau <elodie.gateau.raffeneau@gmail.com>
2025-09-20 14:54:42 +0200

    Résolution bug js leaflet

--- assets/js/trail-map.js
+++ assets/js/trail-map.js
@@ -1,25 +1,30 @@
-document.addEventListener("DOMContentLoaded", () => {
-    if (typeof window.L === "undefined") return;
+const el = document.getElementById("map");
 
-    const el = document.getElementById("map");
-    if (!el) return;
+if (el) {
+    document.addEventListener("DOMContentLoaded", () => {
+        if (typeof window.L === "undefined") return;
 
-    const gpxUrl = el.dataset.gpx;
-    if (!gpxUrl) return;
+        const el = document.getElementById("map");
+        if (!el) return;
 
-    const map = L.map(el);
-    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-        attribution: "© OpenStreetMap contributors",
-    }).addTo(map);
+        const gpxUrl = el.dataset.gpx;
+        if (!gpxUrl) return;
 
-    new L.GPX(gpxUrl, {
-        async: true,
-        marker_options: {
-            startIconUrl: "https://unpkg.com/leaflet-gpx/pin-icon-start.png",
-            endIconUrl: "https://unpkg.com/leaflet-gpx/pin-icon-end.png",
-            shadowUrl: "https://unpkg.com/leaflet-gpx/pin-shadow.png",
-        },
-    })
-        .on("loaded", (e) => map.fitBounds(e.target.getBounds()))
-        .addTo(map);
-});
+        const map = L.map(el);
+        L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+            attribution: "© OpenStreetMap contributors",
+        }).addTo(map);
+
+        new L.GPX(gpxUrl, {
+            async: true,
+            marker_options: {
+                startIconUrl:
+                    "https://unpkg.com/leaflet-gpx/pin-icon-start.png",
+                endIconUrl: "https://unpkg.com/leaflet-gpx/pin-icon-end.png",
+                shadowUrl: "https://unpkg.com/leaflet-gpx/pin-shadow.png",
+            },
+        })
+            .on("loaded", (e) => map.fitBounds(e.target.getBounds()))
+            .addTo(map);
+    });
+}
